092c11e0f246cd16b300440b00432bce8aeda8ec
Rosario Nici <rosarionici95@gmail.com>
2022-09-01 11:30:39 +0200

    Odd or Even

--- file.js
+++ file.js
@@ -3,7 +3,7 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 
 */
-
+/*
 //chiediamo all'utente una parola
 const word = prompt('Scrivi una parola')
 
@@ -27,7 +27,7 @@ function parolaPalindroma() {
 parolaPalindroma(word)
 
 
-
+*/
 
 
 
@@ -44,22 +44,92 @@ Dichiariamo chi ha vinto. */
 
 
 
+
+
+// impostiamo il meccanismo del gioco per il pari
+
+
+let result;
+function playOddOrEven(human, pc) {
+    result = human + pc
+    console.log(result)
+    if (result % 2 === 0) {
+        alert('Hai vinto')
+    } else {
+        alert('Ha vinto il PC')
+    }
+
+}
+function playOddOrEvenInverse(human, pc) {
+    result = human + pc
+    console.log(result)
+    if (result % 2 != 0) {
+        alert('Hai Vinto')
+    } else {
+        alert('Ha vinto IL pc')
+    }
+}
+
+
 // impostimao gli eventi sui pulsanti
+const buttonOdd = document.getElementById('odd')
 
-// const buttonOdd = document.getElementById('odd')
+const buttonEven = document.getElementById('even')
 
-// const buttonEven = document.getElementById('even')
+// creaiamo le funzioni per i bottoni
 
-// let numberUser;
+buttonEven.addEventListener('click',
+    function numberEvenInsert() {
+        const numberEven = parseInt(prompt('Inserisci un numero pari da 1 a 5'))
 
-// buttonEven.addEventListener('click',
-//     function oddOrEven() {
-//         numberUser = parseInt(prompt('Inserisci un numero pari compreso tra 1 a 5 '))
-//         if (!isNaN(numberUser) < 6) {
-//             console.log(numberUser)
-//         } else {
-//             alert(`Attenzione ${numberUser} non è un numero o è superiore a 5 inserisci un numero da 1 a 5`)
-//         }
+        let numberPc = Math.floor(Math.random() * 5 + 1)
+        console.log(numberPc)
 
-//     }
-// )
+        if (!isNaN(numberEven)) {
+
+        } else {
+            alert('Non hai inserito un numero')
+        }
+        if (numberEven > 5) {
+            alert(`hai inserito ${numberEven} che non è compreso tra 1 e 5`)
+        } else {
+        }
+        if (numberEven % 2 === 0) {
+            console.log(numberEven)
+        } else {
+            alert(`${numberEven} non  è un numero pari`)
+        }
+
+    },
+    // playOddOrEven(numberOdd, numberPc)
+)
+
+
+buttonOdd.addEventListener('click',
+    function numberOddInsert() {
+        const numberOdd = parseInt(prompt('Inserisci un numero pari da 1 a 5'))
+        let numberPc = Math.floor(Math.random() * 5 + 1)
+        console.log(numberPc)
+        if (!isNaN(numberOdd)) {
+
+        } else {
+            alert('Non hai inserito un numero')
+        }
+        if (numberOdd > 5) {
+            alert(`hai inserito ${numberOdd} che non è compreso tra 1 e 5`)
+        } else {
+        }
+        if (numberOdd % 2 === 0) {
+            alert(`${numberOdd} non  è un numero dispari`)
+        } else {
+            console.log(numberOdd)
+        }
+    },
+    // playOddOrEvenInverse(numberOdd, numberPc)
+)
+
+
+// creiamo la funzione di generazione numero random per il pc
+
+
+
